d1c701f2be941dd973f091bbf56825010ffc8a1d
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2014-09-05 15:42:15 +0530

    basic visa letter generation working.

--- actions/visa_generator.php
+++ actions/visa_generator.php
@@ -1,7 +1,7 @@
 <?php
 
 $html2pdfClassPath = dirname(__FILE__).'/../includes/html2pdf.class.php';
-$visaLetterTemplate = dirname(__FILE__)."/../templates/invoice.html";
+$visaLetterTemplate = dirname(__FILE__)."/../templates/letter.html";
 
 require_once($html2pdfClassPath);
 
@@ -23,13 +23,18 @@ if (isset($_POST['download_visa']) && isset($_POST['download_visa']) == "downloa
     $organizationName = $_POST["organizationName"];
 
     $content = file_get_contents($visaLetterTemplate);
-    //echo($content);
+
+    // Replace placeholder values
+    $content = str_replace("XX_DATE_XX", date("d-m-Y"), $content);
+    $content = str_replace("XX_FULL_NAME_XX", $fullName, $content);
+    $content = str_replace("XX_PASSPORT_NUM_XX", $passportNumber, $content);
+    $content = str_replace("XX_ORG_NAME_XX", $organizationName, $content);
 
     try
     {
-        $html2pdf = new HTML2PDF('P', 'A4', 'en');
+        $html2pdf = new HTML2PDF('P', 'A4', 'en'); // settings
         $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
-        $html2pdf->Output('exemple10.pdf');
+        $html2pdf->Output('visa_letter_'.$passportNumber.'.pdf');
     }
     catch(HTML2PDF_exception $e) {
         echo $e;
